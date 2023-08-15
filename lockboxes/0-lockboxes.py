d75a1e758305cc9368d52fe17538b0ff4345c784
Javier Icaza <javiicaza@gmail.com>
2023-08-14 20:55:40 -0600

    modifications on code

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 
+
+def join(T, R):
+    res = []
+    for e in R:
+        res += T[e]
+    return res
+
+
 def canUnlockAll(boxes):
-    unlocked = [0]
-    for box_id, box in enumerate(boxes):
-        if not box:
-            continue
-        for key in box:
-            if key < len(boxes) and key not in unlocked and key != box_id:
-                unlocked.append(key)
-    if len(unlocked) == len(boxes):
-        return True
-    return False
+    index = 0
+    total = list(set(boxes[0]) | {0})
+    added = True
+    while added:
+        added = False
+        for j in join(boxes, total[index:]):
+            if j not in total:
+                total.append(j)
+                index += 1
+                added = True
+                print(total)
+                return len(total) == len(boxes)
