f6984a3b9cf4dd75d3f5688bdfbff075d8b107fa
javicaz <javiicaza@gmail.com>
2024-05-03 23:37:52 -0600

    prime game

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -1,93 +1,94 @@
 #!/usr/bin/python3
-"""Prime Game
-Maria and Ben are playing a game. Given a set of consecutive integers starting
-1 up to and including n, they take turns choosing a prime number the
-set and removing that number and its multiples the set. The player that
-cannot make a move loses the game.
+"""
+Defines function that determines the winner after a certain number of rounds
+of playing the Prime Game
 """
 
 
-def print_numbers(numbers):
-    """Prints the numbers in the list"""
-    """print(" leaving", end=" ")"""
-    flag = True
-    for x in range(len(numbers)):
-        if flag is False:
-            """print(", ", end="")"""
-        flag = False
-        """print("{}".format(numbers[x]), end="")"""
-
-
-def isWinner(x: int, nums: list):
-    """Prime Game
-    args:
-        x: number of rounds
-        nums: array of n
-    returns:
-        name of the player that won the most rounds
-        or None in case of a tie
+def isWinner(x, nums):
     """
-    if not nums or x < 1 or min(nums) < 1 or len(nums) != x:
+    Determines the winner after a certain number of rounds
+    of playing the Prime Game
+
+    The Prime Game is a list of consecutive ints starting from 1 up to and
+    including n. Players take turns picking prime numbers, which removes
+    that number and all multiples of that number from the set. The player that
+    has no more prime numbers to choose loses the game.
+
+    Maria and Ben are playing the game, and Maria always goes first.
+
+    parameters:
+        x [int]:
+            the number of rounds
+        nums [list of ints]:
+            list of all ns for each round
+
+    returns:
+        the name of the player that won the most rounds,
+        if both players play optimally,
+        or None, if the winner cannot be determined
+    """
+    Maria = 0
+    Ben = 0
+    if (x < 1 or x != len(nums)):
         return None
+    for n in nums:
+        winner = primeGame(n)
+        if winner == 1:
+            Maria += 1
+        elif winner == 2:
+            Ben += 1
+    if Maria == Ben:
+        return None
+    elif Maria > Ben:
+        return "Maria"
+    return "Ben"
 
-    ben = 0
-    maria = 0
-    for i in range(len(nums)):
-        """print("Round {}: {}".format(i, nums[i]))"""
-        if nums[i] == 1:
-            """print("\t- Ben wins because there are no prime numbers "
-                  "for Maria to choose")"""
-            ben += 1
-            continue
-        numbers = [x for x in range(1, nums[i] + 1)]
 
-        first = True
-        while 1:
-            if first is True:
-                first = False
-                newList = []
-                flag = True
-                """print("\t- Maria picks {} and removes ".
-                      format(numbers[1]), end="")"""
-                for i in range(len(numbers)):
-                    if numbers[i] % 2 == 0 and numbers[i] != 1:
-                        if flag is False:
-                            """print(", ", end="")"""
-                        flag = False
-                        """print("{}".format(numbers[i]), end="")"""
-                        numbers[i] = 0
-                    else:
-                        newList.append(numbers[i])
-                numbers = newList
-                """print_numbers(numbers)"""
+def primeGame(n):
+    """
+    Determines the winner of a single round of the Prime Game
+
+    parameters:
+        n [int]:
+            the maximum number of the set of consecutive ints
+                from 1 up to and including n
+
+    returns:
+        1 if the first player wins the game
+        2 if the second player wins the game
+    """
+    if (n < 1):
+        return None
+    if (n == 1):
+        return (2)
+    numbers = list(range(n + 1))
+    player = 1
+    prime = 2
+    primes_used = []
+    for num in numbers:
+        if (num % prime == 0):
+            numbers.remove(num)
+    primes_used.append(prime)
+    prime = 3
+    while (numbers != [1]):
+        if (player == 1):
+            player = 2
+        else:
+            player = 1
+        for num in numbers:
+            if (num % prime == 0):
+                numbers.remove(num)
+        primes_used.append(prime)
+        prime += 2
+        flag = 1
+        while (flag):
+            for num in primes_used:
+                if (prime % num == 0):
+                    prime += 2
+                    break
             else:
-                """print("\t- Maria picks {} and removes {}"
-                      .format(numbers[1], numbers[1]), end="")"""
-                numbers.remove(numbers[1])
-                print_numbers(numbers)
-            if len(numbers) == 1:
-                """print("\t- Maria wins because there are no prime numbers "
-                      "left for Ben to choose")"""
-                maria += 1
-                break
-            # print(numbers)
-            """print("\t- Ben picks {} and removes {}"
-                  .format(numbers[1], numbers[1]), end="")"""
-            numbers.remove(numbers[1])
-            """print_numbers(numbers)"""
-            if len(numbers) == 1:
-                """print("\t- Ben wins because there are no prime numbers "
-                      "left for Maria to choose")"""
-                ben += 1
-                break
-
-    if ben == maria:
-        return None
-    elif ben > maria:
-        return "Ben"
-    return "Maria"
-
-
-if __name__ == "__main__":
-    isWinner(3, [4, 5, 1])
-
+                flag = 0
+    if (player == 1):
+        return 1
+    return 2
